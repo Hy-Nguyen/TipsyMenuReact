da0bac546c523b0d476ebae948fe98a33a0fc86a
Hy Nguyen <giahy316@gmail.com>
2024-01-25 23:19:55 -0700

    added test directory

---
+++ app/blog/page.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Navbar from "@/components/nav";
+import Header from "@/components/header";
+import Logo from "@/components/imageTest";
+import Chicken from "@/components/chicken";
+import MenuItem from "@/components/menuitem";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div className="flex items-center justify-center flex-wrap bg-amber-100 p-3">
+        <div className="flex items-center flex-shrink-0 text-white mr-6 justify-center">
+          <Logo width={150} height={200} />
+        </div>
+      </div>
+      <Navbar />
+      <div
+        id="container"
+        className="items-center justify-between px-8"
+      >
+        <section id="kfc">
+          <Header text={"Tipsy Chicken"} />
+          <div
+            className="min-w-64 max-w-screen 
+                        md:grid md:grid-cols-2 
+                        lg:grid lg:grid-cols-3 lg:max-w-screen-xl lg:m-auto"
+          >
+            <Chicken />
+          </div>
+        </section>
+        <section id="pre-game">
+          <Header text={"Pre-Game"} />
+          <div
+            className="min-w-64 max-w-screen 
+                        md:grid md:grid-cols-2 
+                        lg:grid lg:grid-cols-3 lg:max-w-screen-xl lg:m-auto"
+          >
+            <MenuItem />
+            <MenuItem />
+          </div>
+        </section>
+        <section id="Party-On">
+          <Header text={"Party-On"} />
+          <div
+            className="min-w-64 max-w-screen 
+                        md:grid md:grid-cols-2 
+                        lg:grid lg:grid-cols-3 lg:max-w-screen-xl lg:m-auto"
+          >
+            <MenuItem />
+            <MenuItem />
+          </div>
+        </section>
+        <section id="Drinks">
+          <Header text={"Drinks"} />
+          <div
+            className="min-w-64 max-w-screen 
+                        md:grid md:grid-cols-2 
+                        lg:grid lg:grid-cols-3 lg:max-w-screen-xl lg:m-auto"
+          >
+            <MenuItem />
+            <MenuItem />
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
